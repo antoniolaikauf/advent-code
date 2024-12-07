8deab990a281cb8182fd0a82ddae0165f7eeb6bc
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-07 13:58:31 +0100

    advent 1

--- 2022/one.c
+++ 2022/one.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    FILE *file = fopen("input.txt", "r");
+    FILE *file = fopen("input.txt", "r"); 
     if (file == NULL)
     {
         printf("file non trovato");
@@ -13,33 +13,31 @@ int main()
     }
     else
     {
-        // buffer è dove viene salvato il file
         int calories = 0;
         int number;
         int indice = 0;
         int length = 250;
         int calories_elf[length];
-        char buffer[100]; // spazio per leggere la riga del file
-        while (fgets(buffer, sizeof(buffer), file))
+        char buffer[100];
+        // buffer è dove verra salvata la linea del file e legge fino a 100 caratteri 
+        while (fgets(buffer, sizeof(buffer), file)) // legge ogni riga fino a 100 caratteri 
         {
-            // printf("%s", buffer);
-            if (sscanf(buffer, "%d", &number) == 1)
+            if (sscanf(buffer, "%d", &number) == 1) // check number 
             {
                 calories += number;
-                // printf("Hai inserito: %d\n", number);
             }
             else
             {
                 calories_elf[indice] = calories;
                 indice++;
                 calories = 0;
-                // printf("Input non valido: '%s'", buffer);
             }
         }
+
         int max = 0;
+
         for (int i = 0; i < length; i++)
         {
-            // printf("%d\n", calories_elf[i]);
             if (max < calories_elf[i])
             {
                 max = calories_elf[i];
