78eb648dba99ddeef66e4f53d567a08273c41ff7
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-14 17:24:01 +0100

    parte tre

--- 2022/tre/tre.c
+++ 2022/tre/tre.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <cstdlib>
+
+void divisione_parola(char parola[], int len, char **s1, char **s2)
+{
+    int left = len / 2;
+    int rigth = len - left;
+    *s1 = (char *)malloc(sizeof(char) * left + 1);
+    *s2 = (char *)malloc(sizeof(char) * rigth + 1);
+}
 
 int main()
 {
@@ -12,6 +21,10 @@ int main()
     while (fgets(buffer, sizeof(buffer), file))
     {
         printf("%s", buffer);
+        buffer[strcspn(buffer, "\n")] = 0;
+        int length = strlen(buffer);
+        char *first, *second;
+        divisione_parola(buffer, length, &first, &second);
     }
     return 0;
 }
