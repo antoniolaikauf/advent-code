c0065c8e7d55ace23c148094e0a977274442493e
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-14 10:43:11 +0100

    due

--- 2022/due/due.c
+++ 2022/due/due.c
@@ -17,12 +17,30 @@ x forbici
 
 int main()
 {
-
     FILE *file = fopen("small.txt", "r");
     if (file == NULL)
         return 0;
 
+    int score;
     char buffer[250];
+    // array due dimensioni 
+    const char combinazioni[][5] = {
+        "A Y",
+        "A X",
+        "A Z",
+        "B Y",
+        "B X",
+        "B Z",
+        "C Y",
+        "C X",
+        "C Z"};
+
+    int length = sizeof(combinazioni) / sizeof(combinazioni[0]);
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("%s\n", combinazioni[i]);
+    }
 
     while (fgets(buffer, sizeof(buffer), file))
     {
