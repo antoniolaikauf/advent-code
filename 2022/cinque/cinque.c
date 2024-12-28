1b41c8263b81b035ff1d430c58ce1790b37e4358
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-12-28 11:53:46 +0100

    cinque

--- 2022/cinque/cinque.c
+++ 2022/cinque/cinque.c
@@ -42,11 +42,28 @@ char *trimwhitespace(char *str)
     return str;
 }
 
-void section(char b[], char one[], char two[], char three[])
-{
-
-    printf("mosse: %s\n", one);
-}
+// void section(char b[], char one[], char two[], char three[])
+// {
+//     while (b[i] != '\0')
+//     {
+//         if (b[i] != 13 && b[i] != '[' && b[i] != ']' && b[i] != 32)
+//         {
+//             if (i % 3 == 1) // Assegna a "one" (ad esempio ogni 3° carattere)
+//             {
+//                 one[idx_one++] = b[i];
+//             }
+//             else if (i % 3 == 2) // Assegna a "two"
+//             {
+//                 two[idx_two++] = b[i];
+//             }
+//             else // Assegna a "three"
+//             {
+//                 three[idx_three++] = b[i];
+//             }
+//         }
+//         i++;
+//     }
+// }
 
 int main()
 {
@@ -56,31 +73,50 @@ int main()
         return 0;
 
     char buffer[250];
-    char array1[6];
-    char array2[6];
-    char array3[6];
+    char array1[50];
+    char array2[50];
+    char array3[50];
     bool mosse = false;
-
+    int idx_one = 0, idx_two = 0, idx_three = 0;
     while (fgets(buffer, sizeof(buffer), file))
     {
-        trimwhitespace(buffer);
-        if (buffer[0] == 13)
+
+        if (buffer[0] == '1' || buffer[0] == 13)
         {
             mosse = true;
             continue;
         }
-
         if (mosse)
         {
             printf("mosse: %s\n", buffer);
         }
         else
         {
-            // removeChar(buffer, '[', ']');
-            section(buffer, array1, array2, array3);
-            // printf("stack: %c\n", buffer[1]);
+            int i = 0;
+            while (buffer[i] != '\0')
+            {
+                if (buffer[i] != 13 && buffer[i] != '[' && buffer[i] != ']' && buffer[i] != 32 && buffer[i] != '1' && buffer[i] != '2' && buffer[i] != '3')
+                {
+                    if (i % 3 == 1) // Assegna a "one"
+                    {
+                        array1[idx_one++] = buffer[i];
+                    }
+                    else if (i % 3 == 2) // Assegna a "two"
+                    {
+                        array2[idx_two++] = buffer[i];
+                    }
+                    else // Assegna a "three"
+                    {
+                        array3[idx_three++] = buffer[i];
+                    }
+                }
+                i++;
+            }
         }
     }
 
+    printf("stack: %s\n", array1);
+    printf("stack: %s\n", array2);
+    printf("stack: %s\n", array3);
     return 0;
 }
