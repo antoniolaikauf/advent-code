ddb151d15e4f64c31074612ad5ec21bf91d1b9e8
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-04 20:27:40 +0100

    tre

--- 2024/tre/tre.c
+++ 2024/tre/tre.c
@@ -22,50 +22,71 @@ int main()
         while (buffer[i] != '\0')
         {
             // printf("indietro %c mezzo %c avanti %c\n", buffer[i - 1], buffer[i], buffer[i + 1]);
-            
+            // controllo se la frase è mul
             if ((buffer[i - 1] == 'm') && (buffer[i] == 'u') && (buffer[i + 1] == 'l'))
             {
-                int j = i + 3;
+                /*
+                    si inizia dal numero perchè se tanto non rispetta il pattern mul()
+                    allora non è da contare questa sequenza
+                */
+                int indexMul = i + 3;
+                /*
+                left serve per cambiare numero da destra asinistra
+                indexNumber serve per inserire il numero nell'array numberLeft e numberRight
+                */
                 bool left = false;
                 int indexNumber = 0;
                 char numberLeft[20] = {0};
                 char numberRight[20] = {0};
-                while ((buffer[j] != ')') && (buffer[j] != 'm'))
+                /*
+                 ciclo finche o non trovo ) o m essendoc he sono i due parametri principali da controllare
+                m per mul e ) per mul(num,num)
+                */
+                
+                while ((buffer[indexMul] != ')') && (buffer[indexMul] != 'm'))
                 {
-                    if (isdigit(buffer[j]) && (left == false))
+                    // controllo se numero
+                    if (isdigit(buffer[indexMul]))
                     {
-                        printf("%c sono a sinista \n", buffer[j]);
-                        numberLeft[indexNumber] = buffer[j];
-                        indexNumber++;
+                        if (left == false)
+                        {
+                            printf("%c sono a sinista \n", buffer[indexMul]);
+                            numberLeft[indexNumber] = buffer[indexMul];
+                            indexNumber++;
+                        }
+                        else
+                        {
+                            printf("%c sono a destra \n", buffer[indexMul]);
+                            numberRight[indexNumber] = buffer[indexMul];
+                            indexNumber++;
+                        }
                     }
-                    else if (isdigit(buffer[j]) && left)
-                    {
-                        printf("%c sono a destra \n", buffer[j]);
-                        numberRight[indexNumber] = buffer[j];
-                        indexNumber++;
-                    }
-                    else if (buffer[j] == ',')
+                    // si cambia numero da destra a sinistra
+                    else if (buffer[indexMul] == ',')
                     {
                         numberLeft[indexNumber] = '\0';
                         left = true;
                         indexNumber = 0;
                     }
-                    else if (isdigit(buffer[j]) == false)
+                    // si rompe il ciclo essendo che è nullo
+                    else if (isdigit(buffer[indexMul]) == false)
                     {
                         checkCorrectSequence = true;
                         printf("numero fake\n");
                         break;
                     }
 
-                    j++;
+                    indexMul++;
                 }
-                numberRight[indexNumber] = '\0';
 
+                numberRight[indexNumber] = '\0';
+                // se si ha trovato un valore nullo allora non si fa niente
                 if (checkCorrectSequence == false)
                 {
                     printf("numero sinistra %s numero a denstra %s\n", numberLeft, numberRight);
                     output += atoi(numberRight) * atoi(numberLeft);
                 }
+                
                 checkCorrectSequence = false;
             }
             i++;
