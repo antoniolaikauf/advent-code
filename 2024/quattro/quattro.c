3f02d46a5e60e41a8775715e7fe8dffe961fa8be
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-08 21:21:32 +0100

    quattro

--- 2024/quattro/quattro.c
+++ 2024/quattro/quattro.c
@@ -37,7 +37,6 @@ void scaleArray(char array[], char value, int index)
         {
             array[i - 1] = array[i];
         }
-
         array[i - 1] = value;
         array[i] = '\0';
     }
@@ -64,35 +63,31 @@ int main()
         trimwhitespace(buffer);
 
         int basicIndex = 0;
-
+        
         if (vertical[0][0] == false)
         {
-            int i = 0;
-            while (buffer[i] != '\0')
-            {
-                indexLast++;
-                i++;
-            }
-            indexLast;
+            int len = strlen(buffer);
+            indexLast = len - 1;
             diagonalIndexTRBl = indexLast;
         }
 
         while (buffer[basicIndex] != '\0')
         {
 
-            if (basicIndex < (indexLast - 3))
+            // if (basicIndex < (indexLast - 3))
+            // {
+            if ((diagonalIndexTRBl - basicIndex) >= 0)
             {
-                if ((diagonalIndexTRBl - basicIndex) >= 0)
-                {
-                    // printf("qua diagonale        %c e qua valore in %i \n", buffer[(diagonalIndexTRBl - basicIndex)], (diagonalIndexTRBl - basicIndex));
-                    scaleArray(diagonalTRBl[basicIndex], buffer[(diagonalIndexTRBl - basicIndex)], verticalIndex);
-                }
-                if ((diagonalIndexTLBR + basicIndex) <= indexLast)
-                {
-                    // printf("qua diagonale        %c e qua valore in %i \n", buffer[(diagonalIndexTLBR + basicIndex)], diagonalIndexTLBR);
-                    scaleArray(diagonalTLBR[basicIndex], buffer[(diagonalIndexTLBR + basicIndex)], verticalIndex);
-                }
+                // printf("qua diagonale        %c e qua valore in %i \n", buffer[(diagonalIndexTRBl - basicIndex)], (diagonalIndexTRBl - basicIndex));
+                scaleArray(diagonalTRBl[basicIndex], buffer[(diagonalIndexTRBl - basicIndex)], verticalIndex);
+                printf("diagonale top right  %s  altro elemento %c\n", diagonalTRBl[basicIndex], buffer[(diagonalIndexTRBl - basicIndex)] );
             }
+            if ((diagonalIndexTLBR + basicIndex) <= indexLast)
+            {
+                // printf("qua diagonale        %c e qua valore in %i \n", buffer[(diagonalIndexTLBR + basicIndex)], diagonalIndexTLBR);
+                scaleArray(diagonalTLBR[basicIndex], buffer[(diagonalIndexTLBR + basicIndex)], verticalIndex);
+            }
+            // }
 
             scaleArray(vertical[basicIndex], buffer[basicIndex], verticalIndex);
 
@@ -101,40 +96,40 @@ int main()
                 output++;
                 printf("trovata oriz XMAS\n");
             }
-            else if (strncmp(&buffer[basicIndex], "SAMX", 4) == 0)
+            if (strncmp(&buffer[basicIndex], "SAMX", 4) == 0)
             {
                 output++;
                 printf("trovata oriz SAMX\n");
             }
-            else if (strncmp(&vertical[basicIndex][0], "XMAS", 4) == 0)
+            if (strncmp(&vertical[basicIndex][0], "XMAS", 4) == 0)
             {
                 output++;
                 printf("trovata XMAS in verticale %s\n", vertical[basicIndex]);
             }
-            else if (strncmp(&vertical[basicIndex][0], "SAMX", 4) == 0)
+            if (strncmp(&vertical[basicIndex][0], "SAMX", 4) == 0)
             {
                 output++;
                 printf("trovata SAMX in verticale %s\n", vertical[basicIndex]);
             }
-            else if (strncmp(&diagonalTLBR[basicIndex][0], "XMAS", 4) == 0)
+            if (strncmp(&diagonalTLBR[basicIndex][0], "XMAS", 4) == 0)
             {
                 output++;
                 printf("trovata XMAS in diagonale top LEFT bottom RIGHT %s\n", diagonalTLBR[basicIndex]);
             }
-            else if (strncmp(&diagonalTLBR[basicIndex][0], "SAMX", 4) == 0)
+            if (strncmp(&diagonalTLBR[basicIndex][0], "SAMX", 4) == 0)
             {
                 output++;
                 printf("trovata XMAS in diagonale top LEFT bottom RIGHT  %s\n", diagonalTLBR[basicIndex]);
             }
-            else if (strncmp(&diagonalTRBl[basicIndex][0], "XMAS", 4) == 0)
+            if (strncmp(&diagonalTRBl[basicIndex][0], "XMAS", 4) == 0)
             {
                 output++;
                 printf("trovata XMAS in diagonale top RIGHT bottom LEFT %s\n", diagonalTRBl[basicIndex]);
             }
-            else if (strncmp(&diagonalTRBl[basicIndex][0], "SAMX", 4) == 0)
+            if (strncmp(&diagonalTRBl[basicIndex][0], "SAMX", 4) == 0)
             {
                 output++;
-                printf("trovata XMAS in diagonale top RIGHT bottom LEFT  %s\n", diagonalTRBl[basicIndex]);
+                printf("trovata SAMX in diagonale top RIGHT bottom LEFT  %s\n", diagonalTRBl[basicIndex]);
             }
 
             // printf("primo indice %s\n", vertical[basicIndex]);
@@ -147,11 +142,12 @@ int main()
         verticalIndex++;
     }
     printf("risultato finale %i", output);
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     printf("qua veritcal %s\n", vertical[i]);
-    //     printf("qua diagonale %s\n", diagonal[i]);
-    // }
 
     return 0;
 }
+
+/*
+nelle diagonali ogni elemento dovrebbe partire un array 
+
+
+*/
