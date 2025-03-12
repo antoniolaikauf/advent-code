091635386ab1df5f09552861ff9cce03c846fa3c
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-12 19:33:05 +0100

    cinque

--- 2024/cinque/cinque.c
+++ 2024/cinque/cinque.c
@@ -2,17 +2,32 @@
 #include <stdio.h>
 #include <string.h>
 
+void costruzioneCoppia(char *coppia, int index, char *buffer)
+{
+    int IndexNumber = 0;
+    for (; IndexNumber < 5 ; IndexNumber++)
+    {
+        coppia[IndexNumber] = buffer[index + IndexNumber];
+    }
+    coppia[IndexNumber] = '\0'; // Termina la stringa
+}
+
 int main()
 {
     FILE *file = fopen("small.txt", "r");
 
     if (file == NULL)
         return 0;
+
     char buffer[250];
-    
+    char combinazioni[250][6] = {0};
     bool changeSolution = false;
+    int indexCombination = 0;
+    int indexCoppie = 0;
+
     while (fgets(buffer, sizeof(buffer), file))
     {
+        int indexDivisionString = 0;
 
         if (buffer[0] == 13)
         {
@@ -22,11 +37,21 @@ int main()
 
         if (changeSolution)
         {
-            printf("%s\n", buffer);
+            char coppia[7] = {0}; // Array per salvare le coppie
+            printf("combinazione %s\n", buffer);
+
+            for (int i = 0; i < strlen(buffer) - 1; i += 3)
+            {
+                costruzioneCoppia(coppia, i, buffer);
+                printf("Coppia generata: %s\n", coppia);
+            }
         }
         else
         {
-            printf("combinazione: %s\n", buffer);
+            buffer[2] = ',';
+            strcpy(combinazioni[indexCombination], buffer);
+            printf("combinazione: %s\n", combinazioni[indexCombination]);
+            indexCombination++;
         }
     }
 
