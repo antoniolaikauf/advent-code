19542e8beb248a5588604f9d2cd13e1aa76733cd
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-19 18:56:34 +0100

    guardia posizione

--- 2024/cinque/cinque.c
+++ 2024/cinque/cinque.c
@@ -1,116 +1,116 @@
-// #include <stdio.h>
-// #include <stdbool.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include <ctype.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-// char *trimwhitespace(char *str)
-// {
-//     char *end;
+char *trimwhitespace(char *str)
+{
+    char *end;
 
-//     // Trim leading space
-//     while (isspace((unsigned char)*str))
-//         str++;
+    // Trim leading space
+    while (isspace((unsigned char)*str))
+        str++;
 
-//     if (*str == 0) // All spaces?
-//         return str;
+    if (*str == 0) // All spaces?
+        return str;
 
-//     // Trim trailing space
-//     end = str + strlen(str) - 1;
-//     while (end > str && isspace((unsigned char)*end))
-//         end--;
+    // Trim trailing space
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
 
-//     // Write new null terminator character
-//     end[1] = '\0';
+    // Write new null terminator character
+    end[1] = '\0';
 
-//     return str;
-// }
+    return str;
+}
 
-// // si mettono i caratteri delle coppie
-// void costruzioneCoppia(char *coppia, int index, char *buffer)
-// {
-//     int IndexNumber = 0;
-//     for (; IndexNumber < 5; IndexNumber++)
-//     {
-//         coppia[IndexNumber] = buffer[index + IndexNumber];
-//     }
-//     coppia[IndexNumber] = '\0'; // Termina la stringa
-// }
+// si mettono i caratteri delle coppie
+void costruzioneCoppia(char *coppia, int index, char *buffer)
+{
+    int IndexNumber = 0;
+    for (; IndexNumber < 5; IndexNumber++)
+    {
+        coppia[IndexNumber] = buffer[index + IndexNumber];
+    }
+    coppia[IndexNumber] = '\0'; // Termina la stringa
+}
 
-// int main()
-// {
-//     FILE *file = fopen("input.txt", "r");
+int main()
+{
+    FILE *file = fopen("input.txt", "r");
 
-//     if (file == NULL)
-//         return 0;
+    if (file == NULL)
+        return 0;
 
-//     char buffer[500];
-//     char combinazioni[2000][6] = {0};
-//     bool changeSolution = false;
-//     int indexCombination = 0;
-//     int indexCoppie = 0;
-//     int output = 0;
+    char buffer[500];
+    char combinazioni[2000][6] = {0};
+    bool changeSolution = false;
+    int indexCombination = 0;
+    int indexCoppie = 0;
+    int output = 0;
 
-//     while (fgets(buffer, sizeof(buffer), file))
-//     {
-//         trimwhitespace(buffer);
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        trimwhitespace(buffer);
 
-//         if (changeSolution)
-//         {
-//             int check = 0;
-//             char coppia[6] = {0}; // Array per salvare le coppie
-//             printf("combinazione %s\n", buffer);
+        if (changeSolution)
+        {
+            int check = 0;
+            char coppia[6] = {0}; // Array per salvare le coppie
+            printf("combinazione %s\n", buffer);
 
-//             for (int i = 0; i < strlen(buffer) - 1; i += 3)
-//             {
-//                 // creazione coppia
-//                 costruzioneCoppia(coppia, i, buffer);
+            for (int i = 0; i < strlen(buffer) - 1; i += 3)
+            {
+                // creazione coppia
+                costruzioneCoppia(coppia, i, buffer);
 
-//                 // se coppia diversa da 4 allora non è una coppia
-//                 if (strlen(coppia) == 4)
-//                     break;
+                // se coppia diversa da 4 allora non è una coppia
+                if (strlen(coppia) == 4)
+                    break;
 
-//                 // ciclo su l'array delle combinazioni per vedere se la coppia è presente
-//                 for (int indexCheck = 0; indexCheck < indexCombination; indexCheck++)
-//                 {
-//                     if (strcmp(coppia, combinazioni[indexCheck]) == 0)
-//                     {
-//                         check++;
-//                         printf("sono uguale %s -> %s\n", coppia, combinazioni[indexCheck]);
-//                         // printf("Coppia generata: %s\n", coppia);
-//                         printf("buffer %s:                         %i\n", buffer, strcmp(coppia, combinazioni[indexCheck]) == 0);
-//                     }
-//                 }
-//             }
-//             // si controlla se le coppie esistono nelle possibili combinazioni
-//             if ((int)(strlen(buffer) / 3) == check)
-//             {
-//                 char number[3] = {buffer[(strlen(buffer) / 2) - 1], buffer[(strlen(buffer) / 2)], '\0'};
-//                 output += atoi(number);
-//                 printf("%s\n\n", number);
-//             }
-//         }
-//         else
-//         {
-//             // si cambia il parametro | con , per rendere il più possibile uguale alla sequenza
-//             buffer[2] = ',';
-//             // copiato la combinazione
-//             strcpy(combinazioni[indexCombination], buffer);
-//             printf("combinazione: %s\n", combinazioni[indexCombination]);
-//             indexCombination++;
-//         }
-//         // si inizia a contollare le sequenze
-//         if (buffer[0] == 13)
-//         {
+                // ciclo su l'array delle combinazioni per vedere se la coppia è presente
+                for (int indexCheck = 0; indexCheck < indexCombination; indexCheck++)
+                {
+                    if (strcmp(coppia, combinazioni[indexCheck]) == 0)
+                    {
+                        check++;
+                        printf("sono uguale %s -> %s\n", coppia, combinazioni[indexCheck]);
+                        // printf("Coppia generata: %s\n", coppia);
+                        printf("buffer %s:                         %i\n", buffer, strcmp(coppia, combinazioni[indexCheck]) == 0);
+                    }
+                }
+            }
+            // si controlla se le coppie esistono nelle possibili combinazioni
+            if ((int)(strlen(buffer) / 3) == check)
+            {
+                char number[3] = {buffer[(strlen(buffer) / 2) - 1], buffer[(strlen(buffer) / 2)], '\0'};
+                output += atoi(number);
+                printf("%s\n\n", number);
+            }
+        }
+        else
+        {
+            // si cambia il parametro | con , per rendere il più possibile uguale alla sequenza
+            buffer[2] = ',';
+            // copiato la combinazione
+            strcpy(combinazioni[indexCombination], buffer);
+            printf("combinazione: %s\n", combinazioni[indexCombination]);
+            indexCombination++;
+        }
+        // si inizia a contollare le sequenze
+        if (buffer[0] == 13)
+        {
 
-//             changeSolution = true;
-//             printf("ora le combinazioni");
-//         }
-//     }
+            changeSolution = true;
+            printf("ora le combinazioni");
+        }
+    }
 
-//     printf("%i\n", output);
-//     return 0;
-// }
+    printf("%i\n", output);
+    return 0;
+}
 
 //------------------------------
 // PARTE DUE sbagliata
--- 2024/sei/sei.c
+++ 2024/sei/sei.c
@@ -1,15 +1,43 @@
-#include <string.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
     FILE *file = fopen("input.txt", "r");
     char buffer[250];
+    int place[250] = {0};
+    int positionGuard = 0;
+    char directionGuard[5] = {'^', 'v', '>', '<', '\0'};
+    int index = 0;
+    int indexCount = 0;
+    char object[] = "#";
+
     while (fgets(buffer, sizeof(buffer), file))
     {
         printf("linea %s", buffer);
+        for (int idxObject = 0; idxObject < (strlen(buffer) - 1); idxObject++)
+        {
+            if (buffer[idxObject] == object[0]) // Compare characters directly
+            {
+                place[index] = indexCount + idxObject;
+                printf("%c --> %i\n", buffer[idxObject], place[index]);
+                index++;
+            }
+            else if ((buffer[idxObject] != object[0]) && (buffer[idxObject] != 46))
+            {
+                positionGuard = indexCount + idxObject;
+                printf("guardia %c --> %i\n", buffer[idxObject], positionGuard);
+            }
+        }
+        indexCount += strlen(buffer);
+    }
+
+    for (int i = 0; i < index; i++)
+    {
+        printf("%i posizione\n", place[i]);
     }
 
     return 0;
