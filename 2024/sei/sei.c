6b9e29f811fcf76421678da6b8c9083a3cda6f72
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-19 19:30:19 +0100

    start move

--- 2024/sei/sei.c
+++ 2024/sei/sei.c
@@ -8,36 +8,53 @@ int main()
 {
     FILE *file = fopen("input.txt", "r");
     char buffer[250];
-    int place[250] = {0};
-    int positionGuard = 0;
+    int place[250][3] = {0};
+    int positionGuard[2] = {0, 0};
     char directionGuard[5] = {'^', 'v', '>', '<', '\0'};
+    int guard = 0;
     int index = 0;
     int indexCount = 0;
     char object[] = "#";
 
+    int heigthMap = 0;
+
     while (fgets(buffer, sizeof(buffer), file))
     {
-        printf("linea %s", buffer);
+        // printf("linea %s\n", buffer);
         for (int idxObject = 0; idxObject < (strlen(buffer) - 1); idxObject++)
         {
             if (buffer[idxObject] == object[0]) // Compare characters directly
             {
-                place[index] = indexCount + idxObject;
-                printf("%c --> %i\n", buffer[idxObject], place[index]);
+                place[index][0] = idxObject;
+                place[index][1] = heigthMap;
+                printf("%c --> %i in ascisse --> %i in ordinate\n", buffer[idxObject], place[index][0], place[index][1]);
                 index++;
             }
-            else if ((buffer[idxObject] != object[0]) && (buffer[idxObject] != 46))
+            else if (buffer[idxObject] == '^' || buffer[idxObject] == 'v' ||
+                     buffer[idxObject] == '>' || buffer[idxObject] == '<')
             {
-                positionGuard = indexCount + idxObject;
-                printf("guardia %c --> %i\n", buffer[idxObject], positionGuard);
+                if (buffer[idxObject] == directionGuard[0])
+                    guard = 0;
+                else if (buffer[idxObject] == directionGuard[1])
+                    guard = 1;
+                else if (buffer[idxObject] == directionGuard[2])
+                    guard = 2;
+                else if (buffer[idxObject] == directionGuard[3])
+                    guard = 3;
+
+                positionGuard[0] = idxObject;
+                positionGuard[1] = heigthMap;
+                printf("%c --> %i in ascisse --> %i in ordinate\n", buffer[idxObject], positionGuard[0], positionGuard[1]);
             }
         }
-        indexCount += strlen(buffer);
+
+        heigthMap++;
     }
 
-    for (int i = 0; i < index; i++)
+    while ((positionGuard[0] > 0 && positionGuard[0] < (strlen(buffer) - 1)) && (positionGuard[1] > 0 && positionGuard[1] < heigthMap))
     {
-        printf("%i posizione\n", place[i]);
+        positionGuard[0]--;
+        printf(" --> %i in ascisse --> %i in ordinate\n",  positionGuard[0], positionGuard[1]);
     }
 
     return 0;
