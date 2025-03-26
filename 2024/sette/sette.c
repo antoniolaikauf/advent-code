db03b97ff7293c16d6d2a11e3a27b5000266be21
antoniolaikauf <antonio.laikauf00@gmail.com>
2025-03-26 19:28:56 +0100

    sette

--- 2024/sette/sette.c
+++ 2024/sette/sette.c
@@ -22,18 +22,28 @@ int main()
         result = atoi(token);
         int indexNumber = 0;
 
-        token = strtok(NULL, " ");
+        token = strtok(NULL, " "); // Get next token before the loop
 
         while (token != NULL)
         {
-            equation[indexNumber] = atoi(token);
+            equation[indexNumber] = atoi(token); // Move this before next strtok
+            // printf("%i\n", equation[indexNumber]);
             indexNumber++;
-            token = strtok(NULL, " ");
+            token = strtok(NULL, " "); // Get next token
         }
+
+        int resultAdd = 0;
+        int resultProd = 1;
 
         for (int i = 0; i < indexNumber; i++)
         {
-            printf("il numero è: %i\n", equation[indexNumber]);
+            resultAdd += equation[i];
+            resultProd *= equation[i];
+        }
+
+        if (resultAdd > result || resultProd > result)
+        {
+            printf("obbiettivo è %i il risultato addizione è: %i il risulato prodotto è:%i \n", result, resultAdd, resultProd);
         }
     }
 }
